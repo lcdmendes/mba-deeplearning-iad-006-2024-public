f0bf88a1b917f0ccebcb6c1b18286459d34f88f2
Luiz Claudio Mendes <54298698+lcdmendes@users.noreply.github.com>
2024-08-16 21:49:12 +0000

    Criando main.py

---
+++ notebooks/main.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI, File, UploadFile
+from pydantic import BaseModel
+import pickle as pk
+import uvicorn
+
+
+app = FastAPI()
+
+class postRequest(BaseModel):
+    features: list[float]
+
+
+class postReponse(BaseModel):
+    prediction: float
+
+
+# Evento OnStart - Carrega modelo
+@app.on_event("startup")
+async def startup_even():
+    global model
+    with open("modelo_ex21a.pkl", "rb") as f:
+        model = pk.load(f)
+
+
+@app.post("/predict", response_model=postReponse)
+async def predict(file: UploadFile):
+    return {"file_size": len(file)}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=30000)
